d4990d4b421a66e1f567f3f9d0a449a80f65f983
vhivestate <garcia.valeria001@gmail.com>
2022-08-25 16:20:37 -0600

    delete items

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -16,12 +16,9 @@ import spinner from '../assets/spinner.gif';
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-
-  const [currentProduct, setCurrentProduct] = useState({});
-
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
-
   const { products, cart } = state;
+  const [currentProduct, setCurrentProduct] = useState({});
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
     if (products.length) {
@@ -36,16 +33,17 @@ function Detail() {
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id);
+  
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 },
+        product: { ...currentProduct, purchaseQuantity: 1 }
       });
     }
   };
@@ -53,9 +51,11 @@ function Detail() {
   const removeFromCart = () => {
     dispatch({
       type: REMOVE_FROM_CART,
-      _id: currentProduct._id,
+      _id: currentProduct._id
     });
   };
+
+  
 
   return (
     <>
@@ -70,8 +70,9 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
+
+            <button 
+              disabled={!cart.find(p => p._id === currentProduct._id)} 
               onClick={removeFromCart}
             >
               Remove from Cart
