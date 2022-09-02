fe0b6a2700317e9e7141a5f683c803fbfe3d35f9
vhivestate <garcia.valeria001@gmail.com>
2022-09-01 19:32:26 -0600

    imprt

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,8 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from "../../utils/helpers";
+
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
