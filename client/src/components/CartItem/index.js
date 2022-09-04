49b6b83d13542c7fb735f09e04442bb5951e886f
vhivestate <garcia.valeria001@gmail.com>
2022-09-03 18:35:08 -0600

    redirect to frontend

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
@@ -13,29 +13,28 @@ const CartItem = ({ item }) => {
       _id: item._id
     });
     idbPromise('cart', 'delete', { ...item });
+
   };
 
-const onChange = (e) => {
-  const value = e.target.value;
+  const onChange = (e) => {
+    const value = e.target.value;
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+      idbPromise('cart', 'delete', { ...item });
 
-  if (value === '0') {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
-  
-    idbPromise('cart', 'delete', { ...item });
-  } else {
-    dispatch({
-      type: UPDATE_CART_QUANTITY,
-      _id: item._id,
-      purchaseQuantity: parseInt(value)
-    });
-  
-    idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+
+    }
   }
-};
-
 
   return (
     <div className="flex-row">
@@ -55,7 +54,7 @@ const onChange = (e) => {
             value={item.purchaseQuantity}
             onChange={onChange}
           />
-           <span
+          <span
             role="img"
             aria-label="trash"
             onClick={() => removeFromCart(item)}
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,6 +16,14 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
 export const QUERY_ALL_PRODUCTS = gql`
   {
     products {
@@ -27,14 +35,6 @@ export const QUERY_ALL_PRODUCTS = gql`
       category {
         name
       }
-    }
-  }
-`;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
     }
   }
 `;
@@ -67,6 +67,4 @@ export const QUERY_USER = gql`
       }
     }
   }
-
-  
 `;
